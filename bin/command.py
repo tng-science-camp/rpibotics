c338c1a32e1e009edd95edf631f55fd732a81e53
auxlepus <auxlepus@gmail.com>
2017-05-11 01:53:42 -0700

    Corrected the import syntax

--- bin/command.py
+++ bin/command.py
@@ -1,28 +1,10 @@
 #!/usr/bin/env python3
-########## DO NOT CHANGE ##########
-import rover
-import time
-  
-#create instance
-myRover=rover.MARSROVER() 
+from rover import Rover
 
-###################################
+myRover = Rover()
 
-#Enter your code below
-#Use "#" to comment out the code
 
-#myRover.measure_humidity() 
-#myRover.measure_temperature() 
-
-#myRover.turn_left(5)
-#myRover.turn_right(5)
-#myRover.go_forward(5)
-#myRover.go_backward(2)
-
-#take photo
-#myRover.capture_image()
-
-#Use this function to control iso and shutter speed (in microsecond, max 6s)
-#myRover.capture_manual_image(100,1000000)
+# Enter your code below
+# Use "#" to comment out the code
 
 
