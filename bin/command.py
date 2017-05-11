8abab9120577c282ea043143fa7b2c6fe8d832e3
auxlepus <auxlepus@gmail.com>
2017-05-11 02:41:20 -0700

    Added a commaand to turn a motor

--- bin/command.py
+++ bin/command.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 from rover import Rover
+import time
 
 myRover = Rover()
 
+myRover.motor1.turn_clockwise()
+myRover.motor2.turn_counter_clockwise()
+time.sleep(3)
+myRover.motor1.stop()
+myRover.motor2.stop()
+
+myRover.motor1.turn_counter_clockwise()
+time.sleep(2)
+myRover.motor1.stop()
 
 # Enter your code below
 # Use "#" to comment out the code
