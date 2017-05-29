8979429445f4db8c3f578fdc0b4174022d94a7b8
auxlepus <auxlepus@gmail.com>
2017-05-29 13:34:48 -0700

    Added parameter type specification.

--- rover/pid.py
+++ rover/pid.py
@@ -1,17 +1,17 @@
 
 
 class PID(object):
-    def __init__(self, kp, ki, kd):
+    def __init__(self, kp: float, ki: float, kd: float):
         self._kp = kp
         self._ki = ki
         self._kd = kd
 
-    def set_gains(self, kp, ki, kd):
+    def set_gains(self, kp: float, ki: float, kd: float):
         self._kp = kp
         self._ki = ki
         self._kd = kd
 
-    def control_delta(self, e0, e1, e2, delta_t):
+    def control_delta(self, e0, e1, e2, delta_t: float):
         delta_u = self._kp * (e2 - e1) + \
                   self._ki * delta_t * e2 + \
                   self._kd * (e2 - 2.0 * e1 + e0) / delta_t
