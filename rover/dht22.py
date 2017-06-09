7f31a3e4d0f83fd6510a67b54e9f79a20a93c18f
auxlepus <auxlepus@gmail.com>
2017-06-09 08:16:03 -0700

    Added the code for the humidity and temperature sensor type DHT22 which is the same as AM2302.

---
+++ rover/dht22.py
@@ -0,0 +1,37 @@
+"""
+
+"""
+import Adafruit_DHT as DHT
+
+
+class DHTSensor(object):
+    def __init__(self, sensor, pin):
+        self._sensor = DHT.AM2302
+        self._pin = pin
+
+    def measure_humidity_and_temperature(self, retries=15, delay_seconds=2):
+        """
+        Measures the humidity and the temperature and retries the measurement
+        with delay_seconds in between the retries. Note that DHT sensor cannot
+        be read faster than once every 2 seconds.
+        :param retries: Number of measurement retries
+        :param delay_seconds: Number of seconds in between retries in seconds
+        :return: (humidity, temperature) in percentage and degree Celsius. 
+                 If measurement fails, return (None, None).
+        """
+        if retries == 0:
+            return DHT.read(self._sensor, self._pin)
+        else:
+            return DHT.read_retry(self._sensor, self._pin,
+                                  retries, delay_seconds)
+
+    def measure_humidity(self, retries=15, delay_seconds=2):
+        humidity, temperature = self.measure_humidity_and_temperature(
+            self, retries, delay_seconds)
+        return humidity
+
+    def measure_temperature(self, retries=15, delay_seconds=2):
+        humidity, temperature = self.measure_humidity_and_temperature(
+            self, retries, delay_seconds)
+        return temperature
+
