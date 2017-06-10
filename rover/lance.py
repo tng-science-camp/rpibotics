d14c7f2d5c69e1322590850a5010093bbfd6aa27
auxlepus <auxlepus@gmail.com>
2017-06-09 23:06:18 -0700

    Added the code for the lance based on the servo class.

---
+++ rover/lance.py
@@ -0,0 +1,21 @@
+from rover.servo import Servo
+
+LANCE_CONFIG = {
+    'gpio_pin': 21,
+    'duty_cycle_open': 3,
+    'duty_cycle_closed': 12
+}
+
+
+class Lance(Servo):
+    def __init__(self, config=LANCE_CONFIG):
+        Servo.__init__(self,
+                       gpio_pin=config['gpio_pin'],
+                       duty_cycle_0=config['duty_cycle_open'],
+                       duty_cycle_180=config['duty_cycle_closed'])
+
+    def arm(self):
+        self.go_to_0()
+
+    def disarm(self):
+        self.go_to_180()
