eeb5bf9aa451bb9293c0c9a38beee191c125c5ac
auxlepus <auxlepus@gmail.com>
2017-06-17 02:14:36 -0700

    Fixes #2 and regactored mobility_system to be evne shorter.

--- rpibotics/mobility_system_sj17.py
+++ rpibotics/mobility_system_sj17.py
@@ -67,12 +67,14 @@ class MobilitySystem(object):
             ((config['initial_duty_cycle']['duty_cycle_left'],),
              (config['initial_duty_cycle']['duty_cycle_right'],))
         self._wheel_circumference = numpy.pi * config['wheel_diameter']
-        self._turn_circumference = numpy.pi * config['wheel_distance']  # [meter]
+        self._turn_circumference = numpy.pi * config[
+            'wheel_distance']  # [meter]
         self._stop = True
 
     def set_initial_duty_cycle(
             self,
-            duty_cycle: Tuple[Tuple[float], Tuple[float]]=((70.0,), (70.0,))):
+            duty_cycle: Tuple[Tuple[float], Tuple[float]] = (
+            (70.0,), (70.0,))):
         self._initial_duty_cycle = duty_cycle
 
     def stop(self):
@@ -106,11 +108,16 @@ class MobilitySystem(object):
                    timeout: float = 30,
                    delta_t: float = 0.01):
         logging.info('Driving forward.')
-        return self.go_straight(target_distance=target_distance,
-                                direction_is_forward=True,
-                                duty_cycle=duty_cycle,
-                                timeout=timeout,
-                                delta_t=delta_t)
+        self.reset()
+        self.enable_stop_when_front_is_blocked()
+
+        target_rotation = numpy.ones((2, 1)) * \
+                          target_distance / self._wheel_circumference
+        return self.move(turn_motors=self.turn_motors_forward,
+                         target_rotation=target_rotation,
+                         duty_cycle=duty_cycle,
+                         timeout=timeout,
+                         delta_t=delta_t)
 
     def go_backward(self,
                     target_distance: float = 0.3,
@@ -118,11 +125,15 @@ class MobilitySystem(object):
                     timeout: float = 5,
                     delta_t: float = 0.01):
         logging.info('Driving backward.')
-        return self.go_straight(target_distance=target_distance,
-                                direction_is_forward=False,
-                                duty_cycle=duty_cycle,
-                                timeout=timeout,
-                                delta_t=delta_t)
+        self.reset()
+
+        target_rotation = numpy.ones((2, 1)) * \
+                          target_distance / self._wheel_circumference
+        return self.move(turn_motors=self.turn_motors_backward,
+                         target_rotation=target_rotation,
+                         duty_cycle=duty_cycle,
+                         timeout=timeout,
+                         delta_t=delta_t)
 
     def turn_right(self,
                    target_angle: float = 90.0,
@@ -130,8 +141,12 @@ class MobilitySystem(object):
                    timeout: float = 30,
                    delta_t: float = 0.001):
         logging.info('Turning right.')
-        return self.turn(target_angle=target_angle,
-                         direction_is_right=True,
+        self.reset()
+        target_rotation = numpy.ones((2, 1)) * \
+                          self._turn_circumference / (360.0 / target_angle) / \
+                          self._wheel_circumference
+        return self.move(turn_motors=self.turn_motors_right,
+                         target_rotation=target_rotation,
                          duty_cycle=duty_cycle,
                          timeout=timeout,
                          delta_t=delta_t)
@@ -142,26 +157,44 @@ class MobilitySystem(object):
                   timeout: float = 30,
                   delta_t: float = 0.001):
         logging.info('Turning left.')
-        return self.turn(target_angle=target_angle,
-                         direction_is_right=False,
+        self.reset()
+        target_rotation = numpy.ones((2, 1)) * \
+                          self._turn_circumference / (360.0 / target_angle) / \
+                          self._wheel_circumference
+        return self.move(turn_motors=self.turn_motors_left,
+                         target_rotation=target_rotation,
                          duty_cycle=duty_cycle,
                          timeout=timeout,
                          delta_t=delta_t)
 
-    def go_straight(self,
-                    target_distance: float = 0.3,
-                    direction_is_forward: bool = True,
-                    duty_cycle: Tuple[Tuple[float], Tuple[float]] = None,
-                    timeout: float = 30,
-                    delta_t: float = 0.01):
+    def turn_motors_forward(self, duty_cycle: Tuple[Tuple[float],
+                                                    Tuple[float]]):
+        self.motor_left.turn_clockwise(duty_cycle[0, 0])
+        self.motor_right.turn_clockwise(duty_cycle[1, 0])
+
+    def turn_motors_backward(self, duty_cycle: Tuple[Tuple[float],
+                                                     Tuple[float]]):
+        self.motor_left.turn_counter_clockwise(duty_cycle[0, 0])
+        self.motor_right.turn_counter_clockwise(duty_cycle[1, 0])
+
+    def turn_motors_right(self, duty_cycle: Tuple[Tuple[float],
+                                                  Tuple[float]]):
+        self.motor_left.turn_clockwise(duty_cycle[0, 0])
+        self.motor_right.turn_counter_clockwise(duty_cycle[1, 0])
+
+    def turn_motors_left(self, duty_cycle: Tuple[Tuple[float],
+                                                 Tuple[float]]):
+        self.motor_left.turn_counter_clockwise(duty_cycle[0, 0])
+        self.motor_right.turn_clockwise(duty_cycle[1, 0])
+
+    def move(self,
+             turn_motors,
+             target_rotation,
+             duty_cycle: Tuple[Tuple[float], Tuple[float]] = None,
+             timeout: float = 10.0,
+             delta_t: float = 0.01):
         start_time = time.time()
 
-        self.reset()
-        if direction_is_forward:
-            self.enable_stop_when_front_is_blocked()
-
-        target_rotation = numpy.ones((2, 1)) * \
-                          target_distance / self._wheel_circumference
         logging.debug('Target Rotation = %s',
                       numpy.array2string(target_rotation).replace('\n', ''))
 
@@ -172,9 +205,12 @@ class MobilitySystem(object):
         e2 = numpy.matrix([[0.0], [0.0]])
         e1 = numpy.matrix([[0.0], [0.0]])
 
+        last_progress_time = time.time()
+
         rotation = numpy.matrix(
             [[self.encoder_left.get_rotations()],
              [self.encoder_right.get_rotations()]])
+        previous_rotation = rotation
 
         self._stop = False
         while not self._stop and time.time() - start_time < timeout:
@@ -197,83 +233,17 @@ class MobilitySystem(object):
                 u2[u2 < 30] = 30.0
                 logging.debug('Control Input = %s',
                               numpy.array2string(u2).replace('\n', ''))
-                if direction_is_forward:
-                    self.motor_left.turn_clockwise(u2[0, 0])
-                    self.motor_right.turn_clockwise(u2[1, 0])
-                else:
-                    self.motor_left.turn_counter_clockwise(u2[0, 0])
-                    self.motor_right.turn_counter_clockwise(u2[1, 0])
+
+                turn_motors(u2)
                 time.sleep(delta_t)
-                rotation = numpy.matrix(
-                    [[self.encoder_left.get_rotations()],
-                     [self.encoder_right.get_rotations()]])
-        distance = rotation * self._wheel_circumference
+                rotation = numpy.matrix([[self.encoder_left.get_rotations()],
+                                         [self.encoder_right.get_rotations()]])
+                if numpy.all(numpy.not_equal(previous_rotation, rotation)):
+                    last_progress_time = time.time()
+                duration_since_last_progress =
+                if time.time() - last_progress_time > 2.0:
+                    logging.debug('No progress has been made in %.1f sec.',
+                                  duration_since_last_progress)
+                    self.stop()
         drive_time = time.time() - start_time
-        return distance, drive_time
-
-    def turn(self,
-             target_angle: float = 90.0,
-             direction_is_right: bool = True,
-             duty_cycle: Tuple[Tuple[float],
-                               Tuple[float]] = ((70.0), (70.0)),
-             timeout: float = 30,
-             delta_t: float = 0.001):
-        start_time = time.time()
-
-        target_rotation = numpy.ones((2, 1)) * \
-                          self._turn_circumference / (360.0 / target_angle) / \
-                          self._wheel_circumference
-        logging.debug('Target Rotation = %s',
-                      numpy.array2string(target_rotation).replace('\n', ''))
-
-        self.reset()
-
-        if duty_cycle is not None:
-            u2 = numpy.matrix(duty_cycle)
-        else:
-            u2 = numpy.matrix(self._initial_duty_cycle)
-        e2 = numpy.matrix([[0.0], [0.0]])
-        e1 = numpy.matrix([[0.0], [0.0]])
-
-        rotation = numpy.matrix(
-            [[self.encoder_left.get_rotations()],
-             [self.encoder_right.get_rotations()]])
-
-        self._stop = False
-
-        while not self._stop and time.time() - start_time < timeout:
-            logging.debug('Measured Rotation = %s',
-                          numpy.array2string(rotation).replace('\n', ''))
-            if numpy.all(numpy.greater_equal(rotation, target_rotation)):
-                logging.debug('Target Rotation = %s has been reached.',
-                              numpy.array2string(target_rotation).replace(
-                                  '\n', ''))
-                self.stop()
-            else:
-                e0 = e1
-                e1 = e2
-                e2 = numpy.matrix([[rotation[1, 0] - rotation[0, 0]],
-                                   [rotation[0, 0] - rotation[1, 0]]]) / 2
-                logging.debug('Rotation Error = %s',
-                              numpy.array2string(e2).replace('\n', ''))
-
-                control_delta = self._pid.control_delta(e0, e1, e2, delta_t)
-                u2 += control_delta
-                u2[u2 > 100] = 100.0
-                u2[u2 < 0] = 0.0
-                logging.debug('Control Input = %s',
-                              numpy.array2string(u2).replace('\n', ''))
-                if direction_is_right:
-                    self.motor_left.turn_clockwise(u2[0, 0])
-                    self.motor_right.turn_counter_clockwise(u2[1, 0])
-                else:
-                    self.motor_left.turn_counter_clockwise(u2[0, 0])
-                    self.motor_right.turn_clockwise(u2[1, 0])
-                time.sleep(delta_t)
-                rotation = numpy.matrix(
-                    [[self.encoder_left.get_rotations()],
-                     [self.encoder_right.get_rotations()]])
-
-        distance = rotation * self._wheel_circumference
-        drive_time = time.time() - start_time
-        return distance, drive_time
+        return rotation, drive_time
